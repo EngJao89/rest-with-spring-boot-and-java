f29454652241f86ffcef7954f6190dfd450b113e
EngJao89 <eng_joao@outlook.com>
2025-11-13 23:33:17 -0300

    refactor: changes logic for return numbers on math controller

--- src/main/java/engjao89/rest_with_spring_boot_and_java/controllers/MathController.java
+++ src/main/java/engjao89/rest_with_spring_boot_and_java/controllers/MathController.java
@@ -12,7 +12,16 @@ public class MathController {
     public Double sum(
             @PathVariable("numberOne") String numberOne,
             @PathVariable("numberTwo") String numberTwo
-    ){
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalAccessException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String number) {
         return 1D;
+    }
+
+    private boolean isNumeric(String number) {
+        return false;
     }
 }
