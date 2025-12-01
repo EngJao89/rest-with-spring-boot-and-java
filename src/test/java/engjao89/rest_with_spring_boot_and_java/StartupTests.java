ab3ef6d3ab31b007a4666bb63c80e20c6f1b6d69
EngJao89 <eng_joao@outlook.com>
2025-12-01 00:14:05 -0300

    feat: create test for using flyway on terminal tests

--- src/test/java/engjao89/rest_with_spring_boot_and_java/StartupTests.java
+++ src/test/java/engjao89/rest_with_spring_boot_and_java/StartupTests.java
@@ -2,8 +2,10 @@ package engjao89.rest_with_spring_boot_and_java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StartupTests {
 
 	@Test
