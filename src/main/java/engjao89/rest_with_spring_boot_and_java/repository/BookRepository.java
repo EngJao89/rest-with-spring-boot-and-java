a14374bbf40ad02cb57c697e41628082841dbcd4
EngJao89 <eng_joao@outlook.com>
2025-12-12 23:00:41 -0300

    feat: create docs, controllers, repository for books.

---
+++ src/main/java/engjao89/rest_with_spring_boot_and_java/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package engjao89.rest_with_spring_boot_and_java.repository;
+
+import engjao89.rest_with_spring_boot_and_java.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {}
