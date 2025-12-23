94a89dad47b678da7aee4c76e60a7d65ab32d9ac
EngJao89 <eng_joao@outlook.com>
2025-12-22 21:58:23 -0300

    feat: create test integraton for cors in application.

--- src/main/java/engjao89/rest_with_spring_boot_and_java/data/dto/V1/PersonDTO.java
+++ src/main/java/engjao89/rest_with_spring_boot_and_java/data/dto/V1/PersonDTO.java
@@ -18,26 +18,10 @@ public class PersonDTO extends RepresentationModel<PersonDTO> implements Seriali
     private static final long serialVersionUID = 1L;
 
     private Long id;
-
-    // @JsonProperty("first_name")
     private String firstName;
-
-    // @JsonProperty("last_name")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String lastName;
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private String phoneNumber;
-
-    @JsonFormat(pattern = "dd/MM/yyyy")
-    private Date birthDay;
     private String address;
-
-    // @JsonIgnore
-    @JsonSerialize(using = GenderSerializer.class)
     private String gender;
-
-    private String sensitiveData;
 
     public PersonDTO() {}
 
@@ -81,39 +65,14 @@ public class PersonDTO extends RepresentationModel<PersonDTO> implements Seriali
         this.gender = gender;
     }
 
-    public Date getBirthDay() {
-        return birthDay;
-    }
-
-    public void setBirthDay(Date birthDay) {
-        this.birthDay = birthDay;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getSensitiveData() {
-        return sensitiveData;
-    }
-
-    public void setSensitiveData(String sensitiveData) {
-        this.sensitiveData = sensitiveData;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        PersonDTO personDTO = (PersonDTO) o;
-        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getPhoneNumber(), personDTO.getPhoneNumber()) && Objects.equals(getBirthDay(), personDTO.getBirthDay()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender()) && Objects.equals(getSensitiveData(), personDTO.getSensitiveData());
+        if (!(o instanceof PersonDTO person)) return false;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddress(), person.getAddress()) && Objects.equals(getGender(), person.getGender());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getPhoneNumber(), getBirthDay(), getAddress(), getGender(), getSensitiveData());
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender());
     }
 }
--- src/test/java/engjao89/rest_with_spring_boot_and_java/config/TestConfigs.java
+++ src/test/java/engjao89/rest_with_spring_boot_and_java/config/TestConfigs.java
@@ -2,4 +2,10 @@ package engjao89.rest_with_spring_boot_and_java.config;
 
 public interface TestConfigs {
     int SERVER_PORT = 8888;
+
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+    String ORIGIN_LOCAL = "http://localhost:8080";
+    String ORIGIN_WEB = "http://localhost:3000";
 }
