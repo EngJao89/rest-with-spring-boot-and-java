b251b0aceb868563bf5c20990b51240a4873c30c
EngJao89 <eng_joao@outlook.com>
2025-12-22 21:24:51 -0300

    feat: enable global cors in general application.

--- src/main/java/engjao89/rest_with_spring_boot_and_java/config/WebConfig.java
+++ src/main/java/engjao89/rest_with_spring_boot_and_java/config/WebConfig.java
@@ -1,12 +1,27 @@
 package engjao89.rest_with_spring_boot_and_java.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                //.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
