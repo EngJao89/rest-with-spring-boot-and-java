d83eef43d4f7b9df2fac663ea6d8d68d1dd70e70
EngJao89 <eng_joao@outlook.com>
2026-01-06 23:44:29 -0300

    refactor: change repositories and model for using paged model.

--- src/main/java/engjao89/rest_with_spring_boot_and_java/model/Person.java
+++ src/main/java/engjao89/rest_with_spring_boot_and_java/model/Person.java
@@ -28,6 +28,10 @@ public class Person implements Serializable {
     @Column(nullable = false, length = 6)
     private String gender;
 
+    @Column(name = "birth_day")
+    @Temporal(TemporalType.DATE)
+    private Date birthDay;
+
     @Column(nullable = false)
     private Boolean enabled;
 
@@ -81,15 +85,23 @@ public class Person implements Serializable {
         this.enabled = enabled;
     }
 
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
-        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddress(), person.getAddress()) && Objects.equals(getGender(), person.getGender()) && Objects.equals(getEnabled(), person.getEnabled());
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddress(), person.getAddress()) && Objects.equals(getGender(), person.getGender()) && Objects.equals(getBirthDay(), person.getBirthDay()) && Objects.equals(getEnabled(), person.getEnabled());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender(), getEnabled());
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender(), getBirthDay(), getEnabled());
     }
 }
