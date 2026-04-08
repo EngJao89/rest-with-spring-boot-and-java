0822669cb19458009cdd9ec143e362685c93add3
EngJao89 <eng_joao@outlook.com>
2026-04-08 08:39:06 -0300

    feat: create file exporter factory on project.

---
+++ src/main/java/engjao89/rest_with_spring_boot_and_java/file/exporter/factory/FileExporterFactory.java
@@ -0,0 +1,30 @@
+package engjao89.rest_with_spring_boot_and_java.file.exporter.factory;
+
+import engjao89.rest_with_spring_boot_and_java.file.exporter.MediaTypes;
+import engjao89.rest_with_spring_boot_and_java.file.exporter.contract.FileExporter;
+import org.apache.coyote.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileExporterFactory {
+
+    private Logger logger = LoggerFactory.getLogger(FileExporterFactory.class);
+
+    @Autowired
+    private ApplicationContext context;
+
+    public FileExporter getExporter(String acceptHeader) throws Exception {
+        if (acceptHeader.equalsIgnoreCase(MediaTypes.APPLICATION_XLSX_VALUE)) {
+            return context.getBean(XlsxExporter.class);
+        } else if (acceptHeader.equalsIgnoreCase(MediaTypes.APPLICATION_CSV_VALUE)) {
+            return context.getBean(CsvExporter.class);
+        } else {
+            throw new BadRequestException("Invalid File Format!");
+        }
+    }
+
+}
